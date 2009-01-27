329c5db2d551c13a16608c232e652d799a84f68b
jturk <jturk@b95ab466-21ef-4d7f-887f-4deb394a2fdb>
2009-01-27 22:25:23 +0000

    changed urls
    git-svn-id: https://source.sunlightlabs.com/sunlightapi/trunk@104 b95ab466-21ef-4d7f-887f-4deb394a2fdb

--- words/urls.py
+++ words/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('words.views',
-    (r'^wordlist/(?P<list_name>\w+)/$', 'word_list'),
-    (r'^remove_stopwords/(?P<list_name>\w+)/$', 'remove_stopwords'),
+    (r'^(?P<list_name>\w+)/$', 'word_list'),
+    (r'^(?P<list_name>\w+)/filter_stopwords/$', 'remove_stopwords'),
 )
 
