b661cd8756d5a5b2fa2830154762952ed2fba8db
jturk <jturk@b95ab466-21ef-4d7f-887f-4deb394a2fdb>
2009-03-10 17:14:26 +0000

    loadlegislators command for loading from apidata
    git-svn-id: https://source.sunlightlabs.com/sunlightapi/trunk@115 b95ab466-21ef-4d7f-887f-4deb394a2fdb

---
+++ legislators/management/commands/loadlegislators.py
@@ -0,0 +1,37 @@
+import csv
+from django.core.management.base import BaseCommand, make_option
+from sunlightapi.legislators.models import Legislator
+
+def dictdiff(a, b):
+    keydiff = set(a.keys()).symmetric_difference(set(b.keys()))
+    if keydiff:
+        print keydiff
+    else:
+        nodiff = True
+        for k,v in a.iteritems():
+            if b[k] != v:
+                if nodiff:
+                    print a['bioguide_id'], a['firstname'], a['lastname']
+                nodiff = False
+                print '  %s %s --> %s' % (k, v, b[k])
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--save', action='store_true', dest='save',
+                    default=False, help='Save changes to database'),)
+    help = "update api data from a csv file"
+
+    def handle(self, *fname, **options):
+        save = options.get('save', False)
+        csvfile = csv.DictReader(open(fname[0]))
+        all_legislators = dict([(l.bioguide_id, l) for l in Legislator.all_legislators.all()])
+
+        for line in csvfile:
+            line['in_office'] = (line['in_office'] == '1')
+            bioguide = line['bioguide_id']
+            leg = all_legislators[bioguide]
+            dictdiff(leg.__dict__, line)
+            if save:
+                leg.__dict__.update(line)
+                leg.save()
+
