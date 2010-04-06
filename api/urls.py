8bf04b3594ea7726092360e2e3bbd5fff4beae4f
James Turk <james.p.turk@gmail.com>
2010-04-06 11:16:47 -0400

    redirect to services/docs

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,6 @@ from django.views.generic.simple import direct_to_template, redirect_to
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('sunlightapi.api.views',
-    # index
-    url(r'^$', direct_to_template, {'template': 'index.html', } ),
+    url(r'^$', redirect_to, {'url': 'http://services.sunlightlabs.com/docs/Sunlight_Congress_API/'}),
     url(r'^register/$', redirect_to, {'url': 'http://services.sunlightlabs.com/accounts/register/'}),
-    url(r'^confirmkey/', direct_to_template, {'template': 'oldconfirm.html', } ),
 )
