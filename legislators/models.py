57d46f03764f9bcd0cd545abe752e9664ed3ea11
James Turk <james.p.turk@gmail.com>
2011-01-05 11:17:51 -0500

    shouldn't have done this without notice

--- legislators/models.py
+++ legislators/models.py
@@ -54,6 +54,7 @@ class Legislator(models.Model):
     fax = PhoneNumberField(blank=True)
     website = models.URLField(blank=True)
     webform = models.URLField(blank=True)
+    email = models.EmailField(blank=True)
 
     # other info
     gender = models.CharField(max_length=1, choices=GENDERS)
@@ -66,6 +67,7 @@ class Legislator(models.Model):
     crp_id = models.CharField(max_length=20)
     bioguide_id = models.CharField(max_length=20, primary_key=True)
     govtrack_id = models.CharField(max_length=20)
+    eventful_id = models.CharField(max_length=20, blank=True)
     twitter_id = models.CharField(max_length=20, blank=True)
     congresspedia_url = models.URLField()
     youtube_url = models.URLField(blank=True)
