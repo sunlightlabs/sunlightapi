eb55053eb1e122fa81e920459601af4818036028
James Turk <james.p.turk@gmail.com>
2011-02-22 11:34:05 -0500

    load latest committee file

--- load_committees.py
+++ load_committees.py
@@ -17,25 +17,19 @@ for data in lines:
     committee = Committee.objects.create(id=com_id, chamber=chamber, parent=parent, name=com)
     if len(coms) == 1:
         parent = committee
-    members = data[2:]
+    members = data[3:]
     for member in members:
-        if member.strip():
-            split = member.strip().split(' ')
-            fname = split[0]
-            lname = split[-1]
-            if lname in ('Jr.','IV'):
-                lname = split[-2]
-            lname = lname.replace('_', ' ')
-
-            # possible titles
-            if chamber == 'Senate':
-                titles = ['Sen']
-            elif chamber == 'House':
-                titles = ['Rep', 'Com', 'Del']
+        member = member.strip()
+        if member:
+            if ' ' in member:
+                split = member.rsplit(' ', 1)
+                fname = split[0]
+                lname = split[1]
             else:
-                titles = ['Rep', 'Sen', 'Com', 'Del']
+                lname = member
 
-            legislators = Legislator.objects.filter(lastname=lname, title__in=titles)
+            legislators = Legislator.objects.filter(lastname=lname,
+                                                    title='Rep')
 
             # handle errors
             if len(legislators) == 0:
@@ -51,16 +45,9 @@ for data in lines:
             else:
                 committee.members.add(legislators[0])
 
-additions = {'R000572': ('HSIF', 'HSIF_cti', 'HSIF_hth', 'HSIG'),
-             'R000575': ('HSAG', 'HSAG_ene', 'HSAG_ldp', 'HSAS', 'HSAS_rdi', 'HSAS_osi', 'HSHM', 'HSHM_bor', 'HSHM_cpr'),
-             'Y000031': ('HSAP', 'HSAP_def', 'HSAP_dva')}
-
-for problem in problems:
-    print problem
-
-for bioguide_id, com_list in additions.iteritems():
-    leg = Legislator.objects.get(pk=bioguide_id)
-    for cname in com_list:
-        c = Committee.objects.get(pk=cname)
-        c.members.add(leg)
-        print 'added %s to %s' % (leg, c)
+#for bioguide_id, com_list in additions.iteritems():
+#    leg = Legislator.objects.get(pk=bioguide_id)
+#    for cname in com_list:
+#        c = Committee.objects.get(pk=cname)
+#        c.members.add(leg)
+#        print 'added %s to %s' % (leg, c)
